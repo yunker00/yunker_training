3c561feffa2a12f575ae162db72b55bcdf53b47d
Yunker <yunker@Yunker-UK.local>
2017-05-20 23:13:01 +0900

    Commit project

---
+++ src/main/java/com/yunker/yunkertraining/YunkerTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.yunker.yunkertraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunkerTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunkerTrainingApplication.class, args);
+	}
+}
